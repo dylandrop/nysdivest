bbe9a24d1e80b020368e13a1fec01fcade30f880
Dylan <dylandrop@gmail.com>
2013-06-12 23:26:56 -0400

    Ok, trying this again, fresh Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nysdivest::Application.config.secret_token = '786edeb92e056e0a87191a635e1310df1922ea7c09b65f7dcaa4cd06263ff09c377dc51f7a0121dffe541697598058d9824c88ec4acd5b1d03f18fc05b57b4c5'
